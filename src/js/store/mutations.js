40105da090aeb23b3b285de94e3298a66b862dd9
anyruizd <anyruizd@gmail.com>
2019-03-17 18:05:49 -0500

    Wire the app all up

---
+++ src/js/store/mutations.js
@@ -0,0 +1,13 @@
+export default {
+  // Mutates the store state
+  addItem(state, payload) {
+    state.items.push(payload)
+
+    return state
+  },
+  clearItem(state, payload) {
+    state.items.splice(payload.index, 1)
+
+    return state
+  }
+}
